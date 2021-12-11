dbc00fb4e99dfe8914acb333952ac6c2875fe1ac
Jacobb626 <76225335+Jacobb626@users.noreply.github.com>
2021-12-11 14:34:37 -0500

    more findobjects

--- UE5.h
+++ UE5.h
@@ -450,6 +450,38 @@ static UObject* FindObject(std::string name)
 	return nullptr;
 }
 
+static UObject* FindObjectEnding(std::string name)
+{
+	for (int32_t i = 0; i < GObjects->NumElements; i++)
+	{
+		auto object = GObjects->GetByIndex(i);
+
+		if (object == nullptr)
+			continue;
+
+		if (object->GetFullName().ends_with(name)) {
+			std::cout << "Found Object Ending: " << object->GetFullName() << std::endl;
+			return object;
+		}
+	}
+}
+
+static UObject* FindObjectStarting(std::string name)
+{
+	for (int32_t i = 0; i < GObjects->NumElements; i++)
+	{
+		auto object = GObjects->GetByIndex(i);
+
+		if (object == nullptr)
+			continue;
+
+		if (object->GetFullName().starts_with(name)) {
+			std::cout << "Found Object Starting: " << object->GetFullName() << std::endl;
+			return object;
+		}
+	}
+}
+
 inline bool ProcessEvent(UObject* pObject, UObject* pFunction, void* pParams) {
 	auto vtable = *reinterpret_cast<void***>(pObject);
 	auto ProcesseventVtable = static_cast<void(*)(void*, void*, void*)>(vtable[0x4B]); if (!ProcesseventVtable) return false;
