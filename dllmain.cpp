538a357950db34c12ec70b49d60e85cc06b331d9
Jacobb626 <76225335+Jacobb626@users.noreply.github.com>
2021-12-18 12:49:35 -0500

    shit

--- dllmain.cpp
+++ dllmain.cpp
@@ -23,6 +23,7 @@ DWORD WINAPI DumpObjectThread(LPVOID param)
 FVector LastEmoteLoc;
 bool bIsEmoting;
 UObject* CurrentEmote;
+bool bIsPickingUp = false;
 
 DWORD WINAPI EmoteCheckThread(LPVOID)
 {
@@ -142,6 +143,9 @@ void* ProcessEventDetour(UObject* pObject, UObject* pFunction, void* pParams)
 
         if (pFunction->GetName().find("ServerHandlePickup") != std::string::npos && FortInventory)
         {
+            if (!bIsPickingUp)
+                bIsPickingUp = true;
+
             struct Params
             {
                 UObject* PickUp;
@@ -156,6 +160,11 @@ void* ProcessEventDetour(UObject* pObject, UObject* pFunction, void* pParams)
                 return NULL;
             }
 
+            auto primQuickbar = reinterpret_cast<FQuickBar*>((uintptr_t)QuickBar + 0x220);
+            auto entries = reinterpret_cast<TArray<FFortItemEntry>*>(__int64(FortInventory) + static_cast<__int64>(0x228) + static_cast<__int64>(0x108));
+
+            std::cout << "CurrentFocusedSlot: " << primQuickbar->CurrentFocusedSlot << std::endl;
+
             auto PickupEntry = reinterpret_cast<FFortItemEntry*>((uintptr_t)params->PickUp + 0x2a0);
             Functions::AddItemToInventoryWithEntry(*PickupEntry, *(int*)((uintptr_t)PickupEntry + 0x0c));
 
@@ -164,6 +173,11 @@ void* ProcessEventDetour(UObject* pObject, UObject* pFunction, void* pParams)
 
         if (pFunction->GetName().find("ServerAttemptInventoryDrop") != std::string::npos && FortInventory)
         {
+            if (bIsPickingUp) {
+                bIsPickingUp = false;
+                return NULL;
+            }
+
             printf("Called remove item!\n");
 
             struct Params
@@ -289,7 +303,7 @@ void* ProcessEventDetour(UObject* pObject, UObject* pFunction, void* pParams)
         if (pFunction->GetName().find("Tick") != std::string::npos)
         {
             if (GetAsyncKeyState(VK_F1) & 0x01) {
-                Functions::SwitchLevel(L"Artemis_Terrain?Game=/Game/Athena/Athena_GameMode.Athena_GameMode_C");
+                Functions::SwitchLevel(L"Apollo_Papaya?Game=/Game/Athena/Athena_GameMode.Athena_GameMode_C");
                 bIsReady = true;
             }
 
